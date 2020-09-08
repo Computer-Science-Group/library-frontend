5d1cae43dfee5c60fccfaa960e0af26431718de3
med-tarek-cs <med.tarek.cs@gmail.com>
2020-09-08 12:04:19 +0100

    update profile

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -5,11 +5,18 @@
 
         <v-card >
           <v-card-title>
+            <v-avatar class="mr-4">
+              <img
+                      src="https://cdn.vuetifyjs.com/images/john.jpg"
+                      alt="John"
+              >
+            </v-avatar>
               {{ user.email }}
           </v-card-title>
           <div>
             <span class="pl-3"> id : {{ user.id}}</span>
           </div>
+          <br>
         </v-card>
       </v-flex>
     </v-layout>
