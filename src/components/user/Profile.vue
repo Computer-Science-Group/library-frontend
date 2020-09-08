047c5d238e2fef7e41da9ff5520580defc5634ff
med-tarek-cs <med.tarek.cs@gmail.com>
2020-09-08 11:48:05 +0100

    refactor auth module

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -1,16 +1,15 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex xs12 sm6 offset-sm3>
-        <v-card>
-          <v-card-text>
-            <div class="text-md-center" v-if="user">
-              <v-icon large color="green darken-2">mdi-check-circle</v-icon>
-              <h1 class="green--text">Login Success</h1>
-              <h4 class="headline mb-0"><b class="red--text">Name :</b> {{ user.name }}</h4>
-              <h4 class="headline mb-0"><b class="red--text">Email :</b> {{ user.email }}</h4>
-            </div>
-          </v-card-text>
+    <v-layout row>
+      <v-flex class="col-12 col-md-6 offset-md-3">
+
+        <v-card >
+          <v-card-title>
+              {{ user.email }}
+          </v-card-title>
+          <div>
+            <span class="pl-3"> id : {{ user.id}}</span>
+          </div>
         </v-card>
       </v-flex>
     </v-layout>
@@ -19,6 +18,15 @@
 
 <script>
   export default {
+    name:'Profile',
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
     computed: {
       user () {
         // return this.$store.getters.user
--- src/main.js
+++ src/main.js
@@ -12,26 +12,31 @@ Vue.config.productionTip = false
 Vue.component('app-alert', AlertCmp)
 
 let app;
-new Vue({
-  store,
-  router,
-  vuetify,
-  render: h => h(App),
-  created(){
-    const config = {
-      apiKey: "AIzaSyBYx5cup538kY0PRGDDs49JNoDDQNhMs9E",
-      authDomain: "vue-library-frontend.firebaseapp.com",
-      databaseURL: "https://vue-library-frontend.firebaseio.com",
-      projectId: "vue-library-frontend",
-      storageBucket: "vue-library-frontend.appspot.com",
-      messagingSenderId: "124698290139",
-      //appId: "1:124698290139:web:c870a525e44282a937a9fe"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(config);
 
-    firebase.auth().onAuthStateChanged(user => {
-      console.log(user)
-    })
+const config = {
+  apiKey: "AIzaSyBYx5cup538kY0PRGDDs49JNoDDQNhMs9E",
+  authDomain: "vue-library-frontend.firebaseapp.com",
+  databaseURL: "https://vue-library-frontend.firebaseio.com",
+  projectId: "vue-library-frontend",
+  storageBucket: "vue-library-frontend.appspot.com",
+  messagingSenderId: "124698290139",
+  //appId: "1:124698290139:web:c870a525e44282a937a9fe"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch('auth/setUser', user)
+  if(!app) {
+    new Vue({
+      store,
+      router,
+      vuetify,
+      render: h => h(App),
+      created(){
+
+      }
+    }).$mount('#app')
+
   }
-}).$mount('#app')
+})
